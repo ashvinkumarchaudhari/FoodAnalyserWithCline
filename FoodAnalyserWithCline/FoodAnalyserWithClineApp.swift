2086f6a38ead9fc6f80aa6cc0a8f23d5aa6248b8
Ashvin <ashvinkumarchaudhari2@gmail.com>
2025-06-02 00:33:13 +0530

    Initial Commit

---
+++ FoodAnalyserWithCline/FoodAnalyserWithClineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAnalyserWithClineApp.swift
+//  FoodAnalyserWithCline
+//
+//  Created by Ashvinkumar Chaudhari on 02.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodAnalyserWithClineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
